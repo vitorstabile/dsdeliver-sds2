5dbc78a770b743c06571114e96236609db42470d
Vitor Garcia <vitor.stabile@hotmail.com>
2021-01-07 20:14:23 -0300

    List Products

--- front-web/src/Orders/index.tsx
+++ front-web/src/Orders/index.tsx
@@ -1,11 +1,24 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import StepsHeader from './StepsHeader';
+import { Product } from './types';
+import ProductsList from './ProductsList';
 import './styles.css';
+import { fetchProducts } from '../api';
 
 function Orders() {
+    const [products, setProducts] = useState<Product []>([]);
+console.log(products);
+
+useEffect(() => {
+    fetchProducts()
+    .then(response => setProducts(response.data))
+    .catch(error => console.log(error))
+}, []);
+
     return (
         <div className="orders-container">
             <StepsHeader />
+            <ProductsList products={products}/>
         </div>
     )
 }
