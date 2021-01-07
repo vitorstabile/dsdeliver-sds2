1e8715db58f75cdc107472ddd1cb517096d560be
Vitor Garcia <vitor.stabile@hotmail.com>
2021-01-07 15:12:16 -0300

    Add Home page

--- front-web/src/App.tsx
+++ front-web/src/App.tsx
@@ -1,11 +1,14 @@
 import React from 'react';
 import './App.css';
+import Navbar from './Navbar';
+import Home from './Home';
 
 function App() {
   return (
-    <div>
-      <h1>Hello</h1>
-    </div>
+    <>
+      <Navbar />
+      <Home />
+    </>
   );
 }
 
